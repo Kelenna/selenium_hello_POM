b3d262aa74aa5784af9396633d3f855c8fe1c475
Elena Chepizhnaya <xiamiamx@gmail.com>
2018-10-28 13:45:37 +0300

    page object added

--- src/test/java/Basic.java
+++ src/test/java/Basic.java
@@ -1,27 +1,32 @@
 import java.io.File;
+import java.util.List;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.SearchPage;
+import pageObjects.SearchResults;
 
 
 public class Basic {
     private WebDriver driver;
     private String baseUrl;
 
-
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", getAppPath());
         driver = new ChromeDriver();
-        baseUrl = "https://www.google.com.ua/";
+        baseUrl = "https://www.google.com";
     }
-
 
     @Test
     public void testSearch() {
         driver.get(baseUrl);
-        driver.findElement(By.xpath("//input[@id='lst-ib']")).sendKeys("Selenium Webdriver");
-        driver.findElement(By.xpath("//input[@value=\"Поиск в Google\"]")).click();
+        SearchPage.SearchField(driver).sendKeys("Hello world!");
+        SearchPage.SubmitSearch(driver).submit();
+        List<WebElement> resultList =  SearchResults.resultsArea(driver).findElements(By.className("g"));
+        resultList.get(4).findElement(By.tagName("a")).click();
+        List<WebElement> helloCheck = driver.findElements(By.xpath("//*[contains(text(), 'hello world')]"));
+        Assert.assertTrue("Not Found!", helloCheck.size() > 0);
     }
 
     private final String getAppPath() {
